318bcd2e4b16f94e343211c33601342c61decc33
Duncan Kipkemoi <kipkemoiduncan7@gmail.com>
2023-05-18 09:40:49 +0300

    done

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    starter = [0, 1]
+    while len(starter) < length:
+        new_number = starter[-1]+starter[-2]
+        starter.append(new_number)
+    print(starter)
+
+
+print_fibonacci(9)
+
+#     sequence = [0, 1]
+#     while len(sequence) < length:
+#         next_number = sequence[-1]+sequence[-2]
+#         sequence.append(next_number)
+#     print(sequence)
+
+
+# print_fibonacci(9)
